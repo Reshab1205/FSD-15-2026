b083e108895f3418364e5110c2306c0cb1194d57
Reshab Kumar <rkmodi12051998@gmail.com>
2026-05-16 20:34:37 +0530

    Javascript Phase 1

---
+++ JavaScript/index.js
@@ -0,0 +1,68 @@
+// console.log('Hello World')
+
+
+
+// var x = 'Reshab';
+// var x = true
+// console.log(x)
+// let y = true;
+//  y = false
+// console.log(y)
+// const c = [1, "Reshab", true, {}, []];
+// const d = {}
+// const c = 28;
+// const c = 29;
+
+// console.log(c)
+
+
+
+
+// const a = 'Reshab'
+// const b = 28;
+// const c = true
+// const obj = {}
+// const arr = []
+// console.log(typeof arr)
+
+// a = 12
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// const a = 'Reshab'
+// const b = 'Kumar'
+
+// Reshab Kumar
+// console.log(a + "       " + b)
+// console.log(true)
+// console.log('Reshab')
+// console.log("Kumar")
+// console.log(`Bhopal`)
+
+// console.log("" == 0)
+
+// const a = 10;
+// const b = '10';
+
+// console.log(true === 1)
+
+const a = true
+if(a == true) {
+    console.log(a)
+    const a = 14;
+    console.log(a)
+    const b = 12;
+} else {
+    console.log('BYe')
+}
+
+
+
+
+
+
+
+
