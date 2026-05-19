1a724af3b2dfdf58480c805e112e66c43754120f
Reshab Kumar <rkmodi12051998@gmail.com>
2026-05-19 20:44:38 +0530

    js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,68 +1,117 @@
-// console.log('Hello World')
+// // console.log('Hello World')
+
+// // var x = 'Reshab';
+// // var x = true
+// // console.log(x)
+// // let y = true;
+// //  y = false
+// // console.log(y)
+// // const c = [1, "Reshab", true, {}, []];
+// // const d = {}
+// // const c = 28;
+// // const c = 29;
+
+// // console.log(c)
+
+// // const a = 'Reshab'
+// // const b = 28;
+// // const c = true
+// // const obj = {}
+// // const arr = []
+// // console.log(typeof arr)
+
+// // a = 12
+
+// // console.log(2+2)
+// // console.log(2-2)
+// // console.log(2*2)
+// // console.log(2/2)
+// // console.log(2%2)
+// // const a = 'Reshab'
+// // const b = 'Kumar'
+
+// // Reshab Kumar
+// // console.log(a + "       " + b)
+// // console.log(true)
+// // console.log('Reshab')
+// // console.log("Kumar")
+// // console.log(`Bhopal`)
+
+// // console.log("" == 0)
+
+// // console.log('Hello',"" + "", 'Hello')
+
+// // const a = 10;
+// // const b = '10';
+
+// // console.log(true === 1)
+
+// // const a = true
+// // if(a == true) {
+// //     // console.log(a)
+// //     const a = 14;
+// //     console.log(a)
+// //     const b = 12;
+// // } else {
+// //     console.log('BYe')
+// // }
+
+// // console.log(a)
 
 
-
-// var x = 'Reshab';
-// var x = true
-// console.log(x)
-// let y = true;
-//  y = false
-// console.log(y)
-// const c = [1, "Reshab", true, {}, []];
-// const d = {}
-// const c = 28;
-// const c = 29;
-
-// console.log(c)
-
-
-
-
-// const a = 'Reshab'
-// const b = 28;
-// const c = true
-// const obj = {}
-// const arr = []
-// console.log(typeof arr)
-
-// a = 12
-
-// console.log(2+2)
-// console.log(2-2)
-// console.log(2*2)
-// console.log(2/2)
-// console.log(2%2)
-// const a = 'Reshab'
-// const b = 'Kumar'
-
-// Reshab Kumar
-// console.log(a + "       " + b)
-// console.log(true)
-// console.log('Reshab')
-// console.log("Kumar")
-// console.log(`Bhopal`)
-
-// console.log("" == 0)
-
-// const a = 10;
-// const b = '10';
-
-// console.log(true === 1)
-
-const a = true
-if(a == true) {
-    console.log(a)
-    const a = 14;
-    console.log(a)
-    const b = 12;
-} else {
-    console.log('BYe')
-}
+// // let a = 11;
+// // if (a === 11) {
+// //     console.log("Hello");
+// // }  else if (a === 15) {
+// //     console.log('Hello4')
+// // }
+// // if (a === 12) {
+// //     console.log("Hello1");
+// // }  
+// // if (a === 14) {
+// //     console.log("Hello2");
+// // } 
+// // else {
+// //     console.log("Bye");
+// // }
 
 
+// // O -> 95-100 %
+// // A+ -> 90-95
+// // A ->85 -90
+// // B+ -> 80 - 85
 
 
+// //  < 50 -> FAIL
+
+// const totalmarks = 505
+
+// const per = totalmarks / 5
+
+// if(100 >= per && per >= 95) {
+//     console.log('O')
+// } 
+// else if (per >= 90) {
+//     console.log('A+')
+// }
+// else if (per >= 85) {
+//     console.log('A')
+// }
+// else if (per >= 80) {
+//     console.log('B+')
+// }
+// else if (per >= 75) {
+//     console.log('B')
+// }
+// else if(per <= 50 && per >= 0) {
+//     console.log('Fail')
+// }
+// else {
+//     console.log('Not a Valid Percentage')
+// }
 
 
-
-
+// const a = 12;
+// if(a === 12) {
+//     console.log(a)
+// }
